25a5628125b873409b9b314731b12b7df49af82d
jkim2473 <jkim2473@bloomberg.net>
2026-03-09 04:05:53 +0000

    Fix vite config for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    allowedHosts: [
-      'spaces-docker-pw-231.dxspda.pw1.bcc.bloomberg.com'
-    ]
-  }
 })
